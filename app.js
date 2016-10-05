dd020b4a16a8ec0c034bffdce69dc86346318f6a
PetarIvancevic <petar.ivancevic171@gmail.com>
2016-10-06 00:11:55 +0200

    added one more route

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 'use strict';
 
-const app = require('express')();
+const express = require('express');
+const app = express();
+const router = express.Router();
 
-app.use(function(req, res, next) {
+router.route('/').get(function(req, res, next) {
   res.status(200).json({message: 'Api development working'});
 });
 
+router.route('/development').get(function(req, res, next) {
+  res.status(200).json({message: 'Api development second route working'});
+});
+
+app.use(router);
 app.listen(3000, function() {
   console.log('Api listening on port 3000')
 });
